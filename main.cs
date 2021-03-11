83fefab2288618a0c79d831ceef6eca06f2536ec
CeolaEastwood <CeolaEastwood@gmail.com>
2021-03-11 18:36:30 +0000

    initialized variable

--- main.cs
+++ main.cs
@@ -1,7 +1,37 @@
 using System;
 
 class MainClass {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+
+public static double DetermineHypotenuse(double side1, double side2)
+  {  
+    double DetermineHypotenuse=0;
+
+    DetermineHypotenuse = Math.Sqrt((side1 * side1) + (side2*side2));
+    
+    return DetermineHypotenuse;
+   
   }
+
+public static void Main (string[] args){
+
+  Console.WriteLine("How many triangles to calculate?");
+  double n = Convert.ToDouble(Console.ReadLine());
+
+  for (int i=0; i<n;i++)
+   {
+    Console.WriteLine("What is side 1?");
+    double num1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("What is side 2?");
+    double num2 = Convert.ToDouble(Console.ReadLine());
+
+    double hypotenuse = DetermineHypotenuse(num1, num2);
+
+    Console.WriteLine("The hypotenuse is " + hypotenuse);
+    }
+  
+
+ }
+  
+  
 }
